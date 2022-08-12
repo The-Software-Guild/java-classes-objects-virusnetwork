7fa8cb70eeb6940766bc7641dac133b6221d8c50
Miles Singleton <milessingleton@hotmail.co.uk>
2022-08-12 07:51:32 +0100

    Added JavaDocs to controller, controller finished.

--- src/main/java/controller/DVDLibraryController.java
+++ src/main/java/controller/DVDLibraryController.java
@@ -7,44 +7,72 @@ import view.DVDLibraryView;
 
 import java.util.List;
 
+/**
+ * Controller class for DVD library
+ *
+ * @author Miles Singleton
+ */
 public class DVDLibraryController {
-    private DVDLibraryView view;
-    private DVDLibraryDao dao;
+    private final DVDLibraryView view;
+    private final DVDLibraryDao dao;
 
+    /**
+     * Constructor for DVD library controller
+     *
+     * @param view used to interact with the view layer
+     * @param dao  used to interact with the data access object
+     */
     public DVDLibraryController(DVDLibraryView view, DVDLibraryDao dao) {
         this.view = view;
         this.dao = dao;
     }
 
-    public void run() throws DVDLibraryDaoException {
+    /**
+     * Runs main program loop
+     */
+    public void run() {
         boolean keepGoing = true;
         int menuSelection = 0;
         while (keepGoing) {
-           menuSelection = this.getMenuSelection();
+            menuSelection = this.getMenuSelection();
 
-            switch (menuSelection) {
-                case 1 -> createDVD();
-                case 2 -> removeDVD();
-                case 3 -> editDVDInfo();
-                case 4 -> viewDVD();
-                case 5 -> searchForDVDByTitle();
-                case 6 -> listDVDs();
-                case 7 -> keepGoing = false;
-                default -> unknownCommand();
+            try {
+                switch (menuSelection) {
+                    case 1 -> createDVD();
+                    case 2 -> removeDVD();
+                    case 3 -> editDVDInfo();
+                    case 4 -> viewDVD();
+                    case 5 -> searchForDVDByTitle();
+                    case 6 -> listDVDs();
+                    case 7 -> keepGoing = false;
+                    default -> unknownCommand();
+                }
+
+            } catch (DVDLibraryDaoException e) {
+                view.displayErrorMessage(e.getMessage());
             }
         }
         exitMessage();
     }
 
+    /**
+     * Used to print menu and get user selection
+     *
+     * @return 1-7 to select menu in run
+     */
     private int getMenuSelection() {
         return view.printMenuAndGetSelection();
     }
 
+    /**
+     * Edit specific DVD info. Ask user what specific field then call function to edit that field.
+     *
+     * @throws DVDLibraryDaoException if library.txt cannot be accessed
+     */
     public void editDVDInfo() throws DVDLibraryDaoException {
         String dvdTitle = view.getDVDTitle();
         int editChoice = view.getEditInfo(dao.getDVD(dvdTitle));
-        switch (editChoice)
-        {
+        switch (editChoice) {
             case 1 -> editDVDTitle(dvdTitle);
             case 2 -> editReleaseDate(dvdTitle);
             case 3 -> editMPAARating(dvdTitle);
@@ -56,47 +84,86 @@ public class DVDLibraryController {
 
     }
 
+    /**
+     * Method for searching DVD by title
+     * 6. Allow the user to search for a DVD by title on requirements for assessment
+     *
+     * @throws DVDLibraryDaoException is thrown when library.txt cannot be found/accessed/written to
+     */
     public void searchForDVDByTitle() throws DVDLibraryDaoException {
         view.displaySearchDVDByTitleBanner();
         DVD dvd = dao.getDVD(view.findDVDByTitle());
         view.isDVDThere(dvd);
     }
 
-    public void editDVDTitle(String oldTitle)
-    {
+    /**
+     * Method for editing title of a DVD
+     *
+     * @param oldTitle old title used for key in hash map in DAO
+     */
+    public void editDVDTitle(String oldTitle) {
         dao.editDVDinfoTitle(oldTitle, view.editDVDTitle());
     }
 
-    public void editReleaseDate(String title)
-    {
-        dao.editDVDinfoReleaseDate(title,view.editDVDReleaseDate());
+    /**
+     * Method for changing release date of a DVD
+     *
+     * @param title used for key in hash map in DAO
+     */
+    public void editReleaseDate(String title) {
+        dao.editDVDinfoReleaseDate(title, view.editDVDReleaseDate());
     }
 
-    public void editMPAARating(String title)
-    {
-        dao.editDVDinfoMPAARating(title,view.editMPAARating());
+    /**
+     * Method for editing MPAA rating of a DVD
+     *
+     * @param title used for key in hash map in DAO
+     */
+    public void editMPAARating(String title) {
+        dao.editDVDinfoMPAARating(title, view.editMPAARating());
     }
 
-    public void editDirector(String title)
-    {
-        dao.editDVDinfoDirector(title,view.editDirector());
+    /**
+     * Method for editing director's name on a DVD
+     *
+     * @param title used for key in hash map in DAO
+     */
+    public void editDirector(String title) {
+        dao.editDVDinfoDirector(title, view.editDirector());
     }
 
-    public void editStudio(String title)
-    {
-        dao.editDVDinfoStudio(title,view.editStudio());
+    /**
+     * Method for editing the studio of a DVD
+     *
+     * @param title used for key in hash map in DAO
+     */
+    public void editStudio(String title) {
+        dao.editDVDinfoStudio(title, view.editStudio());
     }
 
-    public void editUserRating(String title)
-    {
-        dao.editDVDinfoUserRating(title,view.editUserRating());
+    /**
+     * Edit user ratings for a DVD
+     *
+     * @param title used for key in hash map in DAO
+     */
+    public void editUserRating(String title) {
+        dao.editDVDinfoUserRating(title, view.editUserRating());
     }
 
-    public void editDVDNotes(String title)
-    {
-        dao.editDVDNotes(title,view.newDVDNote());
+    /**
+     * Edit the notes of a DVD
+     *
+     * @param title used for key in hash map in DAO
+     */
+    public void editDVDNotes(String title) {
+        dao.editDVDNotes(title, view.newDVDNote());
     }
 
+    /**
+     * Create a new DVD object and add to DVD collection in DAO
+     *
+     * @throws DVDLibraryDaoException thrown if file cannot be accessed
+     */
     public void createDVD() throws DVDLibraryDaoException {
         view.displayCreateDVDBanner();
         DVD newDVD = view.getNewDVDInfo();
@@ -104,26 +171,47 @@ public class DVDLibraryController {
         view.displayCreateSuccessBanner();
     }
 
+    /**
+     * Lists all DVDs in DAO collection
+     *
+     * @throws DVDLibraryDaoException thrown if file cannot be accessed
+     */
     public void listDVDs() throws DVDLibraryDaoException {
         view.displayDisplayAllDVDBanner();
         List<DVD> dvdList = dao.getAllDVDs();
         view.displayDVDLibrary(dvdList);
     }
 
+    /**
+     * View a specific DVD
+     *
+     * @throws DVDLibraryDaoException thrown if file cannot be accessed
+     */
     public void viewDVD() throws DVDLibraryDaoException {
         view.displayDisplayDVDBanner();
         view.displayDVD(dao.getDVD(view.getDVDTitle()));
     }
 
+    /**
+     * Remove given DVD
+     *
+     * @throws DVDLibraryDaoException thrown if file cannot be accessed
+     */
     public void removeDVD() throws DVDLibraryDaoException {
         view.displayRemoveDVDBanner();
         view.displayRemovedDVDResult(dao.removeDVD(view.getDVDTitle()));
     }
 
+    /**
+     * Used to inform user of unknown command
+     */
     private void unknownCommand() {
         view.displayUnknownCommandBanner();
     }
 
+    /**
+     * Used to give user exit message
+     */
     private void exitMessage() {
         view.displayExitBanner();
     }
